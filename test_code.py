94b6196db8938e84dbed4c9359ee9a39ba05d79e
mxzgit <devx.mehdi.studies@gmail.com>
2019-02-06 17:19:39 +0100

    first Good tagging

--- test_code.py
+++ test_code.py
@@ -1,23 +1,4 @@
-# s  = sentence.index('is fun')
-# e  = sentence.rindex('is fun')
-# Is = sentence[:s].count(" ")
-# Ie = sentence[s:e].count(" ") + Is
-
-
-sentence = "mehdi is a mehdi"
-searching = 'mehdi'
-s  = sentence.index(searching)
-e  = sentence.rindex(searching)
-Is = sentence[:s].count(" ")
-Ie = searching.count(" ") + Is
-
-
-
-
-
-
 def pos_calc(sentence,search_str):
-	#print(len(sentence),len(search_str))
 
 	data  = []
 	start = 0
@@ -27,27 +8,22 @@ def pos_calc(sentence,search_str):
 
 	Is = 0
 	Ie = 0
-	
+	s  = 0 
+
 	while (truth):
 		truth = False
-
-		s  = 0 
 
 		if (search_str in sentence[start:end]):
 
 			truth = True
 			s = sentence[start:end].index(search_str)
-			print('"{}"'.format(sentence[start:s+1]))
-			print(sentence[start:s].count(" "))
-			#Is = sentence[start:s].count(" ")+Ie
-			#print(" " in sentence[start-1:s])
-			#Ie += search_str.count(" ") + Is
-			#data.append((Is,Ie,"out_coll"))
-			start += len(search_str)
-			#print(start,s)
-
+			Is = sentence[start:start+s].count(" ")+Ie
+			Ie = search_str.count(" ") + Is
+			data.append((Is,Ie,"out_coll"))
+			start += len(search_str) + len(sentence[start:start+s])
+			
 	return data
 
 
 
-print(pos_calc("mehdi mehdi","mehdi"))
+print(pos_calc("mehdi is a mehdi lsks,lsjdkls zidpzoz mehdi ,azdjad mehdi","mehdi is"))
