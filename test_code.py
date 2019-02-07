9400c6131d009cb44e1296b06a44894ea88a0feb
mxzgit <devx.mehdi.studies@gmail.com>
2019-02-07 09:57:05 +0100

    POS_TAG

--- test_code.py
+++ test_code.py
@@ -23,11 +23,26 @@ def pos_calc(sentence,search_str):
 			s = sentence[start:end].index(search_str)
 			Is = sentence[start:start+s].count(" ")+Ie
 			Ie = search_str.count(" ") + Is
-			data.append((Is,Ie,"out_coll"))
+			data.append((Is,Ie,search_str,"out_coll"))
 			start += len(search_str) + len(sentence[start:start+s])
 			
 	return data
 
 
+def pos_tag(sentence,data):
 
-print(pos_calc("             mehdi is a mehdi lsks, lsjdkls zidpzoz mehdi ,azdjad mehdi","mehdi"))
+	sentence = sentence.lstrip()
+	sentence = sentence.split(" ")
+	
+
+	final_data = []
+	tags = ["O"]*len(sentence)
+
+	for element in data:
+		tags[element[0]] = "B-"+element[3]
+	
+	final_data.append(tags)
+	return final_data
+
+data = pos_calc("mehdi is a mehdi","mehdi")
+print(pos_tag("mehdi is a mehdi",data))
